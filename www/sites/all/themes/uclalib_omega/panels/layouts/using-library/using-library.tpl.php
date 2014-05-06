b50c8b1b5acdc00718d92ff7b3ddab04a6fc908d
cecrs <cecily.borzillo@gmail.com>
2014-05-05 20:29:35 -0500

    UCLALIB-94: add using library layout for news/events tabs config.

---
+++ www/sites/all/themes/uclalib_omega/panels/layouts/using-library/using-library.tpl.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * @file
+ * Template for the Golden layout.
+ *
+ * Variables:
+ * - $css_id: An optional CSS id to use for the layout.
+ * - $content: An array of content, each item in the array is keyed to one
+ * panel of the layout. This layout supports the following sections:
+ */
+?>
+<div<?php print $attributes ?>>
+
+  <?php if (!empty($content['top'])): ?>
+    <div<?php print drupal_attributes($region_attributes_array['top'])?>>
+      <?php print $content['top'] ?>
+    </div>
+  <?php endif; ?>
+
+  <?php if (!empty($content['sidebar_left'])): ?>
+    <div<?php print drupal_attributes($region_attributes_array['sidebar_left'])?>>
+      <?php print $content['sidebar_left'] ?>
+    </div>
+  <?php endif; ?>
+
+  <div class="l-region l-region--main-column">
+    <?php if (!empty($content['main'])): ?>
+      <div<?php print drupal_attributes($region_attributes_array['main'])?>>
+        <?php print $content['main'] ?>
+      </div>
+    <?php endif; ?>
+
+    <?php if (!empty($content['below_main_left'])): ?>
+      <div<?php print drupal_attributes($region_attributes_array['below_main_left'])?>>
+        <?php print $content['below_main_left'] ?>
+      </div>
+    <?php endif; ?>
+
+  </div>
+
+  <?php if (!empty($content['sidebar_right'])): ?>
+    <div<?php print drupal_attributes($region_attributes_array['sidebar_right'])?>>
+      <?php print $content['sidebar_right'] ?>
+    </div>
+  <?php endif; ?>
+
+</div>
