c6d5797a5a6169bb1afcd2db0d89873b233e093c
Adrian Rollett <adrian@bluesparklabs.com>
2014-05-09 16:09:48 -0600

    UCLALIB-26: initial work for "open now" functionality on location pages.

---
+++ www/sites/all/modules/features/uclalib_location/uclalib_location.js
@@ -0,0 +1,37 @@
+
+/**
+ * @file ct_topic_landing_page.js
+ */
+(function ($) {
+
+// Initial page load.
+$(window).load( function() {
+  // Initial population of open now/closed text.
+  checkIfOpen();
+
+  // Update every five minutes.
+  setInterval( function() {
+    checkIfOpen();
+  }, 300*1000);
+
+});
+
+// Function to check if the current location is open now or not.
+var checkIfOpen = function () {
+  var openNowCallback = "/ajax/open_now/" + Drupal.settings.locationNid.nid;
+  $.getJSON(openNowCallback).done(function(data) {
+    if (typeof data.nodes[0] !== 'undefined') {
+      if (data.nodes[0].node.nid == Drupal.settings.locationNid.nid) {
+        $('.open-now-pane').html('<span class="location-open-now">Open Now</span>');
+      }
+    }
+    else {
+      $('.open-now-pane').html('<span class="location-closed">Closed</span>');
+    }
+  })
+  .fail(function() {
+    console.log("Could not get location opening status.");
+  })
+}
+
+})(jQuery);
