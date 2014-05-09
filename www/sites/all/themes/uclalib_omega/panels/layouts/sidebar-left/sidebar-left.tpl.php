3d6976cb90b399cd4e95837d37bf0e0474f890db
James Wilson <jrwilson3@gmail.com>
2014-05-09 18:00:04 +0200

    UCLALIB-58: Recreate Sidebar Left panel layout as a 2-col brick layout.

---
+++ www/sites/all/themes/uclalib_omega/panels/layouts/sidebar-left/sidebar-left.tpl.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * @file
+ * Template for the Sidebar left grid layout.
+ *
+ * Variables:
+ * - $css_id: An optional CSS id to use for the layout.
+ * - $content: An array of content, each item in the array is keyed to one
+ * panel of the layout. This layout supports the following sections:
+ */
+?>
+<div<?php print $attributes ?>>
+
+  <?php if (!empty($content['top'])): ?>
+    <div<?php print drupal_attributes($region_attributes_array['top'])?>>
+      <?php print $content['top'] ?>
+    </div>
+  <?php endif; ?>
+
+  <?php if (!empty($content['sidebar_left'])): ?>
+    <div<?php print drupal_attributes($region_attributes_array['sidebar_left'])?>>
+      <?php print $content['sidebar_left'] ?>
+    </div>
+  <?php endif; ?>
+
+  <div class="l-region l-region--main-column">
+    <?php if (!empty($content['main'])): ?>
+      <div<?php print drupal_attributes($region_attributes_array['main'])?>>
+        <?php print $content['main'] ?>
+      </div>
+    <?php endif; ?>
+
+    <?php if (!empty($content['below_main_left'])): ?>
+      <div<?php print drupal_attributes($region_attributes_array['below_main_left'])?>>
+        <?php print $content['below_main_left'] ?>
+      </div>
+    <?php endif; ?>
+
+    <?php if (!empty($content['below_main_right'])): ?>
+      <div<?php print drupal_attributes($region_attributes_array['below_main_right'])?>>
+        <?php print $content['below_main_right'] ?>
+      </div>
+    <?php endif; ?>
+
+    <?php if (!empty($content['secondary'])): ?>
+      <div<?php print drupal_attributes($region_attributes_array['secondary'])?>>
+        <?php print $content['secondary'] ?>
+      </div>
+    <?php endif; ?>
+
+    <?php if (!empty($content['below_secondary_left'])): ?>
+      <div<?php print drupal_attributes($region_attributes_array['below_secondary_left'])?>>
+        <?php print $content['below_secondary_left'] ?>
+      </div>
+    <?php endif; ?>
+
+    <?php if (!empty($content['below_secondary_right'])): ?>
+      <div<?php print drupal_attributes($region_attributes_array['below_secondary_right'])?>>
+        <?php print $content['below_secondary_right'] ?>
+      </div>
+    <?php endif; ?>
+
+  </div>
+</div>
