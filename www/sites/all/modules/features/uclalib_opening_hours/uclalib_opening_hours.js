e144d0fe0c64824bba5511c99b489b1b6c6d828d
Adrian Rollett <adrian@bluesparklabs.com>
2014-05-22 16:34:18 -0600

    UCLALIB-41: add pager functionality to hours table, fix a couple of bugs.

---
+++ www/sites/all/modules/features/uclalib_opening_hours/uclalib_opening_hours.js
@@ -0,0 +1,29 @@
+/**
+ * @file uclalib_opening_hours.js
+ */
+(function ($) {
+
+Drupal.behaviors.openingHours = {
+	attach: function (context, settings) {
+
+		// Attach pager behavior to hours arrow links.
+		$('.opening-hours-link').once(function() {
+			$(this).click(function() {
+				addWeekPager($(this));
+				return false;
+			});
+		});
+	}
+};
+
+var addWeekPager = function (element) {
+	var nid = $(element).attr('nid');
+	var date = ($(element).attr('date'));
+	var tableCallback = "/ajax/opening_hours_table/" + nid + "/" + date;
+	var table = $(element).closest('table');
+	table.load(tableCallback, function() {
+		Drupal.attachBehaviors($(this));
+	});
+}
+
+})(jQuery);
