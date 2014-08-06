6803aa37ed835b160729b3cb690db0d296daeb87
Nicolò Caruso <nicolo@istos.it>
2014-08-06 12:37:41 +0200

    UCLALIB-137: Add slide numbers in Living Library gallery

--- www/sites/all/themes/uclalib_omega/js/uclalib-omega.behaviors.js
+++ www/sites/all/themes/uclalib_omega/js/uclalib-omega.behaviors.js
@@ -73,3 +73,15 @@
   };
 
 })(jQuery);
+
+function fancybox_beforeshow() {
+  this.title = '<div class="title">' + (this.title ? '' + this.title + '' : '') + '</div><div class="slide-numbers" style="position: absolute; top: 0; left: 0;">' + (this.index + 1) + ' of ' + this.group.length + '</div>';
+}
+
+function fancybox_aftershow() {
+  width = jQuery('.fancybox-title .child').width();
+  image_width = jQuery('.fancybox-image').width();
+  image_height = jQuery('.fancybox-image').height();
+  jQuery('.fancybox-title .slide-numbers').css("margin-left", -(image_width - width - 390) + "px");
+  jQuery('.fancybox-title .slide-numbers').css("top", -(image_height + 30) + "px");
+}
