c146917db186882036d857637d71dcec2011aaec
Casey Grzecka <c@sey.gr>
2017-01-20 14:57:03 -0800

    DRUP-1124: #ready adds text to social links (#382)
    * DRUP-1124: #ready adds text to social links
    
    This was done by switching to a more robust library
    
    commit 2c542926e76081a94117eba8d64b733397b2c08c
    Author: Casey Grzecka <cgrzecka@library.ucla.edu>
    Date:   Thu Jan 19 07:06:39 2017 +0000
    
        DRUP-1124: changes order of share icons, email icon to envelope, and minor secletor change and jQuery syntax
    
    commit de11198e009ace4abedebae7a496d07bc94e0752
    Author: Casey Grzecka <cgrzecka@library.ucla.edu>
    Date:   Thu Jan 19 06:19:18 2017 +0000
    
        DRUP-1124: moves template to template file and loads external js/css via attached property
    
    commit 0883a8ce7c7bf9ebd4abed528bf8d5ca05a949fb
    Author: Casey Grzecka <cgrzecka@library.ucla.edu>
    Date:   Wed Jan 18 19:52:52 2017 +0000
    
        DRUP-1124: WIP on using jssocials to replace addthis TODO: remove nasty inline template
    
    * dropping beta

---
+++ www/sites/all/modules/features/uclalib_system/plugins/content_types/templates/uclalib-system-meta-social.tpl.php
@@ -0,0 +1,17 @@
+<div id="share"></div>
+
+<script>
+  (function ($) {
+    $("#share").jsSocials({
+      showLabel: true,
+      shares: [
+        "twitter", "facebook", { share: "email", logo: "fa fa-envelope-o" }
+      ]
+    });
+  }(jQuery));
+</script>
+
+<style type="text/css">
+  .jssocials-share-link { border-radius: 50%; }
+  .jssocials-share-label { display: none; }
+</style>
